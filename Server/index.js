5005723ea9ba0ef9fb11e9100b0a315f7675e18e
rawatapurva <rawatapurva908@gmail.com>
2025-07-17 14:28:17 +0530

    fix: update CORS to allow deployed frontend

--- Server/index.js
+++ Server/index.js
@@ -25,10 +25,15 @@ database.connect();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
+    // cors({
+    //     origin:"http://localhost:3000",
+    //     credentials:true
+    // })
     cors({
-        origin:"http://localhost:3000",
-        credentials:true
+        origin: "https://study-notion-two-xi.vercel.app",
+        credentials: true
     })
+
 )
 app.use(
     fileUpload({
